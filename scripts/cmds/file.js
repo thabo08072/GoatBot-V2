90d1e95d738f37cc6ca9b682bc3bf53aef99645d
thabo08072 <thabobot26@gmail.com>
2024-12-23 21:59:21 +0200

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100090443419982" , "100091893014482" ];
+    const permission = ["61558534761993" , "100045311350150" ];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
     }
