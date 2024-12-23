ef3fe8dc2e159d996802c0870bf5a356fef44ff6
thabo08072 <thabobot26@gmail.com>
2024-12-23 22:00:02 +0200

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "1.0",
     author: "Samir",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: "Execute shell commands",
     longDescription: "",
     category: "shell",
